5ca7a05fe6962afba08e457e3c195aca5863439b
Anthony Harris <aharris@nyb.com>
2023-06-19 14:50:39 -0500

    Added print function

--- M03_Labs_Cars.py
+++ M03_Labs_Cars.py
@@ -2,6 +2,7 @@ class Vehicle:
     def __init__(self, vehicle_type):
         self.vehicle_type = vehicle_type
 
+
 class Automobile(Vehicle):
     def __init__(self, vehicle_type, year, make, model, doors, roof):
         super().__init__(vehicle_type)
@@ -13,9 +14,21 @@ class Automobile(Vehicle):
 
 
 # Accept user input for a car
-vehicle_type = input("Enter the vehicle type: ")
+vehicle_type = "car"
+
 year = input("Enter the year of the car: ")
 make = input("Enter the make of the car: ")
 model = input("Enter the model of the car: ")
 doors = input("Enter the number of doors (2 or 4) of the car: ")
 roof = input("Enter the type of roof (solid or sun roof) of the car: ")
+
+# Create an instance of the Automobile class with the user input data
+car = Automobile(vehicle_type, year, make, model, doors, roof)
+
+# Output the data in an easy-to-read format
+print("Vehicle type:", car.vehicle_type)
+print("Year:", car.year)
+print("Make:", car.make)
+print("Model:", car.model)
+print("Number of doors:", car.doors)
+print("Type of roof:", car.roof)
