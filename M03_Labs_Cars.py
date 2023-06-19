55f0dd68583df106e20b9581c5f9e8b0b8fa4d09
Anthony Harris <aharris@nyb.com>
2023-06-19 14:22:01 -0500

    Added class and super class, added user input

--- M03_Labs_Cars.py
+++ M03_Labs_Cars.py
@@ -1,1 +1,21 @@
-print("Establish Repo")
+class Vehicle:
+    def __init__(self, vehicle_type):
+        self.vehicle_type = vehicle_type
+
+class Automobile(Vehicle):
+    def __init__(self, vehicle_type, year, make, model, doors, roof):
+        super().__init__(vehicle_type)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+
+# Accept user input for a car
+vehicle_type = input("Enter the vehicle type: ")
+year = input("Enter the year of the car: ")
+make = input("Enter the make of the car: ")
+model = input("Enter the model of the car: ")
+doors = input("Enter the number of doors (2 or 4) of the car: ")
+roof = input("Enter the type of roof (solid or sun roof) of the car: ")
